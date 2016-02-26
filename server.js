d9739488f201ee3e0c13a8dd773cdb380cded71c
dschinkel <dschinkel@gmail.com>
2016-02-26 01:29:57 -0600

    serving the html page statically, no need for app.get()

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-    res.render('index.html');
-});
 
 app.listen(4000, function () {
     console.log('Example app listening on port 4000!');
